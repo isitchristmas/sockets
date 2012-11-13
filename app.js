1a6bfba54e83f25fa17a2c43a36acdd9ee473e07
Eric Mill <konklone@gmail.com>
2012-11-12 19:59:32 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+function main(req, res) {
+  res.send("Main");
+}
+
+
+/****** */
+
+var express = require('express')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 4000);
+  app.use(express.logger('dev'));
+  app.use(app.router);
+});
+
+app.get('/', main);
+
+
+// Start server
+
+var startServer = function() {
+  http.createServer(app).listen(app.get('port'), function(){
+    console.log("Express %s server listening on port %s", app.settings.env, app.get('port'));
+  });
+}
+
+app.configure('development', function() {
+  app.use(express.errorHandler());
+
+  require('reloader')({
+    watchModules: true,
+    onReload: startServer
+  });
+});
+
+app.configure('production', startServer);
